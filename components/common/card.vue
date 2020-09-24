2b43a1f3cae07062461be19fe4cdd18421de23a3
zyszzx <1002203386@qq.com>
2020-09-24 11:24:14 +0800

    个人中心

---
+++ components/common/card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <h1>{{card.title}}</h1>
+    <p>{{card.amount}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    card:{
+      type:Object,
+      required:true
+    }
+  },
+  
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 1rem;
+  height: 10rem;
+}
+</style>
