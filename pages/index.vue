cd19728b611c0b4bbdaa9e33f6fd6f77956c0da2
hhemin <heminfind@aliyun.com>
2020-09-24 11:16:35 +0800

    首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="headtop">
-12321
+  <div>
+    <div class="columns main">
+      <div class="column is-6 left">1</div>
+      <div class="column center">2</div>
+      <div class="column is-3 right">3</div>
+    </div>
   </div>
 </template>
 
 <script>
+// @name 股市
   export default {
     layout:'LMenu'
   }
 </script>
 
 <style lang="scss" scoped>
-
+.main {
+  background-color: yellowgreen !important;
+  width: 100%;
+  // height: 100vh;
+  min-height: 100vh;
+  margin: 0 !important;
+  padding: 0 !important;
+  .left {
+    height: 100vh;
+    background-color: blue;
+  }
+  .center {
+    height: 100vh;
+  }
+  .right {
+    height: 100vh;
+    background-color: yellow;
+  }
+}
 </style>
